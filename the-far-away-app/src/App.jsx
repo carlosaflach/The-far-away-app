5f2a7d07cb311e0673bedebf4b45099b91a5cf22
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-02 15:00:21 -0300

    feat: updating some styles

--- the-far-away-app/src/App.jsx
+++ the-far-away-app/src/App.jsx
@@ -1,21 +1,15 @@
-import { Form } from "./components/Form";
-import { Logo } from "./components/Logo";
-import { PackingList } from "./components/PackingList";
+import { Form } from './components/Form';
+import { Logo } from './components/Logo';
+import { PackingList } from './components/PackingList';
+import { Stats } from './components/Stats';
 
 export default function App() {
   return (
-    <div>
+    <div className='app'>
       <Logo />
       <Form />
       <PackingList />
+      <Stats />
     </div>
   );
 }
-
-
-
-
-
-
-
-
