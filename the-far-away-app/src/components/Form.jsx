136006aafa63595f1f97e4715937258acc3217c1
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-02 15:10:20 -0300

    feat: start to implement the handleSubmit on the form

--- the-far-away-app/src/components/Form.jsx
+++ the-far-away-app/src/components/Form.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+
 export const Form = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log('newItem', newItem);
+
+    setDescription('');
+    setQuantity(1);
+  };
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3> What do you need for your tripe?</h3>
-      <select name='' id=''>
+      <select name='' id='' value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, index) => index + 1).map((el, i) => (
           <option value={el} key={el}>
             {el}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input type='text' placeholder='Item...' value={description} onChange={(e) => setDescription(e.target.value)} />
       <button>Add</button>
     </form>
   );
