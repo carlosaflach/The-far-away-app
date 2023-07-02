22e128ddf6786d3439eb22f7a8f15404791742e5
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-02 15:00:54 -0300

    feat: building the form component

--- the-far-away-app/src/components/Form.jsx
+++ the-far-away-app/src/components/Form.jsx
@@ -1,7 +1,16 @@
 export const Form = () => {
   return (
-    <div className='add-form'>
+    <form className='add-form'>
       <h3> What do you need for your tripe?</h3>
-    </div>
+      <select name='' id=''>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((el, i) => (
+          <option value={el} key={el}>
+            {el}
+          </option>
+        ))}
+      </select>
+      <input type='text' placeholder='Item...' />
+      <button>Add</button>
+    </form>
   );
 };
