6c5dd054139e21d7c85eea38f8b9bd9f22877874
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-02 22:51:47 -0300

    feat: filtering itens by order, description and packed order

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import Item from './Item';
 
 export const PackingList = ({ itens, onAddItem, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItens;
+
+  if (sortBy === 'input') sortedItens = itens;
+
+  if (sortBy === 'description') sortedItens = itens.slice().sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed') sortedItens = itens.slice().sort((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className='list'>
       <ul>
-        {itens.map((item) => (
+        {sortedItens.map((item) => (
           <Item key={item.id} item={item} onAddItem={onAddItem} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
+      <div className='actions'>
+        <select name='' id='' value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
