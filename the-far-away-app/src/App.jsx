95c2aeb33d698938a23ead5ab4a51a2eaaf0a7f6
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-02 22:29:12 -0300

    feat: Calculating Statistics as Derived State

--- the-far-away-app/src/App.jsx
+++ the-far-away-app/src/App.jsx
@@ -6,7 +6,7 @@ import { Stats } from './components/Stats';
 
 export default function App() {
   const initialItems = [
-    { id: 1, description: 'Passports', quantity: 2, packed: true },
+    { id: 1, description: 'Passports', quantity: 2, packed: false },
     { id: 2, description: 'Socks', quantity: 12, packed: false },
   ];
   const [itens, setItens] = useState(initialItems);
@@ -24,7 +24,7 @@ export default function App() {
       <Logo />
       <Form onAddItem={setItens} />
       <PackingList itens={itens} onDeleteItem={removeItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats itens={itens} />
     </div>
   );
 }
--- the-far-away-app/src/components/Stats.jsx
+++ the-far-away-app/src/components/Stats.jsx
@@ -1,7 +1,23 @@
-export const Stats = () => {
+export const Stats = ({ itens }) => {
+  if (!itens.length)
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list 🚀 </em>
+      </p>
+    );
+
+  const numItems = itens.length;
+  const numPacked = itens.filter((item) => item.packed).length;
+
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <footer className="stats">
-      <em> You have x Items on your list, and you already pack X%</em>
+    <footer className='stats'>
+      <em>
+        {percentage === 100
+          ? ' You got everything! Ready to go ✈️'
+          : `🧳 You have ${numItems} items on your list, and you already pack ${numPacked} ${percentage}%`}{' '}
+      </em>
     </footer>
   );
 };
