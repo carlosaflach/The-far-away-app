58dc8656def0b1e7081a07ed5e9ad1ae5536dc43
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-13 23:30:07 -0300

    refactor: Change the initial items to a empty arrray.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import { PackingList } from './components/PackingList';
 import { Stats } from './components/Stats';
 
 export default function App() {
-  const initialItems = [
-    { id: 1, description: 'Passports', quantity: 2, packed: false },
-    { id: 2, description: 'Socks', quantity: 12, packed: false },
-  ];
+  const initialItems = [];
   const [itens, setItens] = useState(initialItems);
 
   const removeItem = (id) => {
